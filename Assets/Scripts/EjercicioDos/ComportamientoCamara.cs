a87e44ec489d9ab1b5e48f418e22ff6b5156c4ab
ANavarro96 <alejandronavarro96@hotmail.com>
2024-01-08 11:16:39 +0000

    Initial commit

---
+++ Assets/Scripts/EjercicioDos/ComportamientoCamara.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ComportamientoCamara : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* TODO: Lanzar un rayo al pulsar el botón, y modificar el destino de nuestro agente */
+
+    }
+
+}
